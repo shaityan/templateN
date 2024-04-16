e241a1e6f8f38b5bc3ed8f8f1f3096e3e5ebd691
shaityan <kulyk000770@gmail.com>
2024-04-16 11:05:54 +0300

    re

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-jQuery(function ($) {
-    function getDeviceType() {
-        const userAgent = navigator.userAgent.toLowerCase();
-        const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
-
-        console.log(userAgent); // "mobile" or "desktop"
-        if (isMobile) {
-            const pageHeight = 2*document.documentElement.clientHeight;
-            $('.topper-table-mob').removeAttr('style');
-            $('.topper-table-mob').attr('style', 'height:' + pageHeight + 'px;');
-
-        } else {
-            $('.topper-table').removeAttr('style');
-        }
-    }
-
-    window.onload = function () {
-        getDeviceType();
-    }
-});
+// jQuery(function ($) {
+//     function getDeviceType() {
+//         const userAgent = navigator.userAgent.toLowerCase();
+//         const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
+//
+//         console.log(userAgent); // "mobile" or "desktop"
+//         if (isMobile) {
+//             const pageHeight = document.documentElement.clientHeight;
+//             $('.topper-table-mob').removeAttr('style');
+//
+//
+//         } else {
+//             $('.topper-table').removeAttr('style');
+//
+//         }
+//     }
+//
+//     window.onload = function () {
+//         getDeviceType();
+//     }
+// });
 
 
