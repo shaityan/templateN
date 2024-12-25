85afc9cb75a1392c955d672d6649560e881550ab
Кулык Анастасия Андреевна <kulyk000770@gmail.com>
2024-12-25 14:07:53 +0300

    g

--- js/main.js
+++ js/main.js
@@ -3,20 +3,22 @@ jQuery(function ($) {
         const userAgent = navigator.userAgent.toLowerCase();
         const isMobile = /mobile|iphone|ipad|meizu|samsung|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
 
-        console.log(userAgent); // "mobile" or "desktop"
+        // console.log(userAgent); // "mobile" or "desktop"
         if (isMobile) {
             const pageHeight = document.documentElement.clientHeight;
             $('.topper-table-mob').removeAttr('style');
-
+            
 
         } else {
             $('.topper-table').removeAttr('style');
-
+            
         }
+        
     }
 
     window.onload = function () {
         getDeviceType();
+        // window.location.reload();
     }
 });
 
